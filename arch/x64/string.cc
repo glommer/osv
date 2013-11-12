8a181629f9d496997f1e677fd5e9f432fd1a2b2d
Glauber Costa <glommer@cloudius-systems.com>
2013-11-12 15:21:55 +0400

    string: add fixups for memcpy operations
    When we start using the JVM balloon, our memcpy could fail for valid reasons
    when the JVM is moving memory that is now in an unmapped region. To handle that,
    register a fixup that will trigger a JVM call when the fault happens. If all goes
    well, we will be able to continue normally.
    
    Signed-off-by: Glauber Costa <glommer@cloudius-systems.com>

--- arch/x64/string.cc
+++ arch/x64/string.cc
@@ -9,11 +9,38 @@
 #include <string.h>
 #include <stdint.h>
 #include "cpuid.hh"
+#include "debug.hh"
+#include "exceptions.hh"
+#include "prio.hh"
 
 extern "C"
 void *memcpy_base(void *__restrict dest, const void *__restrict src, size_t n);
 extern "C"
 void *memset_base(void *__restrict dest, int c, size_t n);
+
+static inline __always_inline void
+repmovsq(void *__restrict &dest, const void *__restrict &src, size_t &n)
+{
+    asm volatile
+       ("1: \n\t"
+        "rep movsq\n\t"
+        ".pushsection .memcpy_decode, \"ax\" \n\t"
+        ".quad 1b, 8\n\t"
+        ".popsection\n"
+            : "+D"(dest), "+S"(src), "+c"(n) : : "memory");
+}
+
+static inline __always_inline void
+repmovsb(void *__restrict dest, const void *src, size_t n)
+{
+    asm volatile
+       ("1: \n\t"
+        "rep movsb\n\t"
+        ".pushsection .memcpy_decode, \"ax\" \n\t"
+        ".quad 1b, 1\n\t"
+        ".popsection\n"
+            : "+D"(dest), "+S"(src), "+c"(n) : : "memory");
+}
 
 extern "C"
 void *memcpy_repmov_old(void *__restrict dest, const void *__restrict src, size_t n)
@@ -21,17 +48,17 @@ void *memcpy_repmov_old(void *__restrict dest, const void *__restrict src, size_
     auto ret = dest;
     auto nw = n / 8;
     auto nb = n & 7;
-    asm volatile("rep movsq" : "+D"(dest), "+S"(src), "+c"(nw) : : "memory");
-    asm volatile("rep movsb" : "+D"(dest), "+S"(src), "+c"(nb) : : "memory");
+    repmovsq(dest, src, nw);
+    repmovsb(dest, src, nb);
     return ret;
 }
 
 
 extern "C"
-void *memcpy_repmov(void *__restrict dest, const void *__restrict src, size_t n)
+void *memcpy_repmov(void *dest, const void *src, size_t n)
 {
     auto ret = dest;
-    asm volatile("rep movsb" : "+D"(dest), "+S"(src), "+c"(n) : : "memory");
+    repmovsb(dest, src, n);
     return ret;
 }
 
@@ -47,6 +74,57 @@ void *(*resolve_memcpy())(void *__restrict dest, const void *__restrict src, siz
 void *memcpy(void *__restrict dest, const void *__restrict src, size_t n)
     __attribute__((ifunc("resolve_memcpy")));
 
+extern memcpy_decoder memcpy_decode_start[], memcpy_decode_end[];
+
+static void sort_memcpy_decoder() __attribute__((constructor(init_prio::sort)));
+
+static void sort_memcpy_decoder()
+{
+    std::sort(memcpy_decode_start, memcpy_decode_end);
+}
+
+void memcpy_decoder::memcpy_fixup(exception_frame *ef, size_t fixup)
+{
+    int direction = ef->rflags & (1 << 10);
+    if (!direction) {
+        ef->rdi += fixup;
+        ef->rsi += fixup;
+    } else {
+        ef->rsi -= fixup;
+        ef->rdi -= fixup;
+    }
+    ef->rcx -= fixup / _word_size;
+}
+
+unsigned char *memcpy_decoder::dest(exception_frame *ef)
+{
+    return reinterpret_cast<unsigned char *>(ef->rdi);
+}
+
+unsigned char *memcpy_decoder::src(exception_frame *ef)
+{
+    return reinterpret_cast<unsigned char *>(ef->rsi);
+}
+
+ulong memcpy_decoder::size(exception_frame *ef)
+{
+    return ef->rcx;
+}
+
+memcpy_decoder::memcpy_decoder(ulong pc, ulong word_size)
+    : _pc(pc), _word_size(word_size)
+{
+}
+
+memcpy_decoder *memcpy_find_decoder(exception_frame *ef)
+{
+    memcpy_decoder v{ef->rip, 0};
+    auto dec = std::lower_bound(memcpy_decode_start, memcpy_decode_end, v);
+    if (dec != memcpy_decode_end && ((*dec) == ef->rip)) {
+        return &*dec;
+    }
+    return nullptr;
+}
 
 extern "C"
 void *memset_repstos_old(void *__restrict dest, int c, size_t n)
