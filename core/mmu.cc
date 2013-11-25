a90d82057c249f8cc6c319b99bd2b608da93eb7d
Glauber Costa <glommer@cloudius-systems.com>
2013-11-25 23:58:26 +0400

    vma_fault: propagate exception frame to fault handlers
    We suddenly stop propagating the exception frame down the vma_fault path.
    Propagate it further so we can use it for the JVM balloon.
    
    Signed-off-by: Glauber Costa <glommer@cloudius-systems.com>

--- core/mmu.cc
+++ core/mmu.cc
@@ -879,7 +879,7 @@ void vm_fault(uintptr_t addr, exception_frame* ef)
             return;
         }
 
-        vma->fault(addr);
+        vma->fault(addr, ef);
     }
     trace_mmu_vm_fault_ret(addr, ef->error_code);
 }
@@ -947,7 +947,7 @@ error vma::sync(uintptr_t start, uintptr_t end)
     return no_error();
 }
 
-void vma::fault(uintptr_t addr)
+void vma::fault(uintptr_t addr, exception_frame *ef)
 {
     auto hp_start = ::align_up(_start, huge_page_size);
     auto hp_end = ::align_down(_end, huge_page_size);
@@ -1001,7 +1001,7 @@ error file_vma::sync(uintptr_t start, uintptr_t end)
     return make_error(err);
 }
 
-void file_vma::fault(uintptr_t addr)
+void file_vma::fault(uintptr_t addr, exception_frame *ef)
 {
     abort("trying to fault-in file-backed vma");
 }
