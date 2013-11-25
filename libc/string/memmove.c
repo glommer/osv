2cd233ad6220a3f3d686b59b5f027673a0ae95e6
Glauber Costa <glommer@cloudius-systems.com>
2013-11-25 23:54:31 +0400

    string: implement memmove using memcpy
    The current implementation of memmove is a PITA (I mean the bread, of course)
    to decode if a fault happens. We have very little control of where exactly in
    the code the fault happens, therefore it is difficult to reason about it. This
    patch implements memmove in terms of memcpy + memcpybackwards. For those, we
    have specific fixups in the possible fault sites, that will allow us to decode
    the faults with ease.
    
    Signed-off-by: Glauber Costa <glommer@cloudius-systems.com>

--- libc/string/memmove.c
+++ libc/string/memmove.c
@@ -1,5 +1,7 @@
 #include <string.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <osv/string.h>
 
 #define WT size_t
 #define WS (sizeof(WT))
@@ -10,27 +12,8 @@ void *memmove(void *dest, const void *src, size_t n)
 	const char *s = src;
 
 	if (d==s) return d;
-	if (s+n <= d || d+n <= s) return memcpy(d, s, n);
+	if (d < s || s+n <= d || d+n <= s) return memcpy(d, s, n);
 
-	if (d<s) {
-		if ((uintptr_t)s % WS == (uintptr_t)d % WS) {
-			while ((uintptr_t)d % WS) {
-				if (!n--) return dest;
-				*d++ = *s++;
-			}
-			for (; n>=WS; n-=WS, d+=WS, s+=WS) *(WT *)d = *(WT *)s;
-		}
-		for (; n; n--) *d++ = *s++;
-	} else {
-		if ((uintptr_t)s % WS == (uintptr_t)d % WS) {
-			while ((uintptr_t)(d+n) % WS) {
-				if (!n--) return dest;
-				d[n] = s[n];
-			}
-			while (n>=WS) n-=WS, *(WT *)(d+n) = *(WT *)(s+n);
-		}
-		while (n) n--, d[n] = s[n];
-	}
-
+	memcpy_backwards(dest, src, n);
 	return dest;
 }
