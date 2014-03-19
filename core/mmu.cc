6501bdee4900d7bc2e53f2e3e4a59b810dfab3f6
Glauber Costa <glommer@cloudius-systems.com>
2014-03-19 08:36:29 -0300

    map_jvm: update the balloon object on address reuse
    If we are reusing an address, it might be that we are doing for the same
    balloon - move-to-self scenario, or it might be that simply there is another
    balloon that is moved to the address where previously another balloon was held,
    but before we had the chance to close it. In the later case, the balloon object
    - which is used to compute offsets in the object is wrong, and we will copy
    things to unpredictable locations. Need to update it correctly.
    
    Signed-off-by: Glauber Costa <glommer@cloudius-systems.com>

--- core/mmu.cc
+++ core/mmu.cc
@@ -1499,6 +1499,10 @@ ulong map_jvm(unsigned char* jvm_addr, size_t size, size_t align, balloon_ptr b)
         assert(v != &*vma_list.end());
         assert(v->has_flags(mmap_jvm_heap) | v->has_flags(mmap_jvm_balloon));
         if (v->has_flags(mmap_jvm_balloon) && (v->addr() == addr)) {
+            jvm_balloon_vma *j = static_cast<jvm_balloon_vma *>(&*v);
+            if (&*j->_balloon != &*b) {
+                j->_balloon = b;
+            }
             return 0;
         }
     }
